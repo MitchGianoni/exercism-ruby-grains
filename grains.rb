00f72de62939bb4dabfb81a232df897da8e9cd1e
MitchGianoni <mitchgianoni@yahoo.com>
2018-05-27 19:20:14 -0400

    solved total method

--- grains.rb
+++ grains.rb
@@ -1,5 +1,21 @@
+require 'pry'
+
 class Grains
   def self.square(num)
-    x = 2**(num-1)
+    if num == 0 || num < 0 || num > 64
+      raise ArgumentError, 'invalid input'
+    else
+      x = 2**(num-1)
+    end
+  end
+
+  def self.total
+    grains = []
+
+    for x in 0..63
+      grains[x] = 2**x
+    end
+
+    grains.sum
   end
 end
